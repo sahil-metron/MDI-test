013904b47ba2c377f00663cd8ee4a8d76f4906fc
sahil-metron <108385016+sahil-metron@users.noreply.github.com>
2022-07-13 14:11:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+
+import websockets
+
+
+async def handler(websocket):
+    while True:
+        try:
+            message = await websocket.recv()
+        except websockets.ConnectionClosedOK:
+            break
+        print(message)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
